8824fa1e27f4c40dc145c52df34fe3d6b84be0b4
Patrice Gauthier <patgauth@gmail.com>
2022-03-11 16:27:00 -0500

    basic chat

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -4,13 +4,29 @@ defmodule ChatWeb.RoomLive do
 
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
-    Logger.info(room_id)
-    {:ok, assign(socket, room_id: room_id)}
+    topic = "room:" <> room_id
+
+    ChatWeb.Endpoint.subscribe(topic)
+
+    {:ok,
+     assign(socket,
+       room_id: room_id,
+       topic: topic,
+       messages: ["You joined the chat"],
+       temporary_assigns: [messages: []]
+     )}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
     Logger.info(message: message)
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    Logger.info(payload: message)
+    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
   end
 end
