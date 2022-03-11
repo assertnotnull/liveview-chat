3d2d1f07a485696c80fa29c5ba9ca9fc8d2d46c4
Patrice Gauthier <patgauth@gmail.com>
2022-03-11 16:56:55 -0500

    set uuid to messages

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -5,14 +5,13 @@ defmodule ChatWeb.RoomLive do
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
-
-    ChatWeb.Endpoint.subscribe(topic)
+    if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
 
     {:ok,
      assign(socket,
        room_id: room_id,
        topic: topic,
-       messages: ["You joined the chat"],
+       messages: [%{uuid: UUID.uuid4(), content: "You joined the chat"}],
        temporary_assigns: [messages: []]
      )}
   end
@@ -20,6 +19,7 @@ defmodule ChatWeb.RoomLive do
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
     Logger.info(message: message)
+    message = %{uuid: UUID.uuid4(), content: message}
     ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, socket}
   end
